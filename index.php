120d750555cf0329145e929eed7dbd645f7f0072
NassimBenchenni <nassim.benchenni@gmail.com>
2026-02-05 09:45:17 +0100

    Index modif

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<main>
+  <body>
+    <h1>Mon Portefolio</h1>
+      <hr>
+    <h2>Nassim Benchenni</h2>
+    <h3>Projet Web</h3>
+    <h4>CV</h4>
+    <a href="presentation.html"> Ma page présentation</a>
+    <br>
+    <a href="liste.html" *>Mon Arborescence</a>
+  </body>
+<footer>
+  <a>DATE :</a>
+  <p>Auteur</p>
+</footer>
+</main>
+</html>
 <?php echo "Enfin en ligne !"; ?>
