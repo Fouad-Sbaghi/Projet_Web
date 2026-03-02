a761a8062140c72ff7d9bd19c0093d149716b40f
NassimBenchenni <nassim.benchenni@gmail.com>
2026-03-02 12:23:09 +0100

    passage w3school + responsive

---
+++ view/cv.php
@@ -0,0 +1,9 @@
+<main>
+
+<div class="cv">
+
+
+
+</div>
+
+</main>
--- view/header.php
+++ view/header.php
@@ -3,15 +3,36 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../view/css/style.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="<?php echo $racine ?>view/css/style.css">
 </head>
 <body>
-<header>
-    <div class="banniere">
-      <nav>
-        <a href="<?php echo $racine ?>index.php">Accueil</a>
-        <a href="<?php echo $racine ?>control/contact.php">Contact</a>
-        <a href="<?php echo $racine ?>control/utilisateur.php">Login</a>
-      </nav>
-    </div>
-</header>
+
+<div class="w3-bar w3-indigo w3-card banniere">
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="toggleMobileMenu()">&#9776;</a>
+  
+  <a href="<?php echo $racine ?>index.php" class="w3-bar-item  w3-hide-small">Accueil</a>
+  <a href="<?php echo $racine ?>control/recherche_cv.php" class="w3-bar-item w3-hide-small">Recherche des CV</a>
+  <a href="<?php echo $racine ?>control/contact.php" class="w3-bar-item w3-hide-small">Contact</a>
+  <a href="<?php echo $racine ?>control/utilisateur.php" class="w3-bar-item w3-hide-small">Login</a>
+  
+</div>
+
+<div id="mobileMenu" class="w3-bar-block w3-indigo w3-hide w3-hide-large w3-hide-medium">
+  <a href="<?php echo $racine ?>index.php" class="w3-bar-item w3-button">Accueil</a>
+  <a href="<?php echo $racine ?>control/recherche_cv.php" class="w3-bar-item w3-button">Recherche des CV</a>
+  <a href="<?php echo $racine ?>control/contact.php" class="w3-bar-item w3-button">Contact</a>
+  <a href="<?php echo $racine ?>control/utilisateur.php" class="w3-bar-item w3-button">Login</a>
+</div>
+
+<script>
+// Fonction pour ouvrir/fermer le menu sur mobile
+function toggleMobileMenu() {
+  var x = document.getElementById("mobileMenu");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+</script>
