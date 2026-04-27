51ab5346ff4bc0386a5d345fa89b6262be461552
Fouad-Sbaghi <fouad.sbaghi7@gmail.com>
2026-04-27 11:45:15 +0200

    feat: add client documentation page and remove redundant footer links

--- admin/view/footer.php
+++ admin/view/footer.php
@@ -1,10 +1,6 @@
 </div>
 <footer class="w3-container w3-indigo w3-center w3-padding-16">
   <p class="w3-text-white">&copy; 2026 Fouad SBAGHI - Nassim Benchenni</p>
-  <p>
-      <a href="<?php echo $racine ?>mentionslegales" class="w3-text-white">Mentions Légales & CGV</a> | 
-      <a href="<?php echo $racine ?>sitemap" class="w3-text-white">Plan du site</a>
-  </p>
 </footer>
 </body>
 
